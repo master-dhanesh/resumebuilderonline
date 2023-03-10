266d5559f4078e0a62e29d178011a5d51e68b6f7
master-dhanesh <dhanesh1296@gmail.com>
2023-03-10 21:18:19 +0530

    Schemas

---
+++ api/models/resumeModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const resumeModel = new mongoose.Schema(
+    {
+        presonalInfo: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+        education: {
+            type: Array,
+            required: [true, "Education is required"],
+        },
+        skill: {
+            type: Array,
+            required: [true, "Skill is required"],
+        },
+        project: [],
+        experience: [],
+        interest: [],
+    },
+    { timestamps: true }
+);
+
+const Resume = mongoose.model("resume", resumeModel);
+
+module.exports = Resume;
+
+/*
+*UserSchema[name, contact,email, profile pic, links, ResumeSchema] 
+*ResumeSchema [ 
+    Personal Info(UserSchema), 
+    Education(Institute Name, Degree, Board, Year, Grades), 
+    Skills(a,b,c,d,Certificate), 
+    Projects(Name, Technology Used, Description, Deployed/share link), 
+    Experience(Company Name, Designation, From-To), 
+    Interests&Hobbies(a,b,c,d, certificate)
+    ]
+*/
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -37,6 +37,7 @@ const userModel = new mongoose.Schema(
                 url: "https://images.unsplash.com/photo-1529641484336-ef35148bab06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
             },
         },
+        resumes: [{ type: mongoose.Schema.Types.ObjectId, ref: "resume" }],
         otp: String,
     },
     { timestamps: true }
